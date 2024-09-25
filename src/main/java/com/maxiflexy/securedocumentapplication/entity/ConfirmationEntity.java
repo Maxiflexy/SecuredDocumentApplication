8940b5d1637512964998168fda4a2a9ed69fdf74
Maxiflexy <onyemax247@gmail.com>
2024-09-25 16:29:38 +0100

    created confirmation entity

---
+++ src/main/java/com/maxiflexy/securedocumentapplication/entity/ConfirmationEntity.java
@@ -0,0 +1,39 @@
+package com.maxiflexy.securedocumentapplication.entity;
+
+import com.fasterxml.jackson.annotation.*;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.UUID;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
+
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "confirmations")
+@JsonInclude(NON_DEFAULT)
+public class ConfirmationEntity extends Auditable {
+
+    private String key;
+
+    @OneToOne(targetEntity = UserEntity.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    @JsonProperty("user_id")
+    private UserEntity userEntity;
+
+    public ConfirmationEntity(UserEntity userEntity){
+        this.key = UUID.randomUUID().toString();
+        this.userEntity = userEntity;
+    }
+}
