e965c8f399c032d3fc7ba312a5ae1d4ee2983238
Maxiflexy <onyemax247@gmail.com>
2024-09-28 05:41:36 +0100

    updated user service impl

--- src/main/java/com/maxiflexy/securedocumentapplication/Application.java
+++ src/main/java/com/maxiflexy/securedocumentapplication/Application.java
@@ -1,13 +1,45 @@
 package com.maxiflexy.securedocumentapplication;
 
+import com.maxiflexy.securedocumentapplication.domain.RequestContext;
+import com.maxiflexy.securedocumentapplication.entity.RoleEntity;
+import com.maxiflexy.securedocumentapplication.enums.Authority;
+import com.maxiflexy.securedocumentapplication.repository.RoleRepository;
+import com.maxiflexy.securedocumentapplication.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(RoleRepository roleRepository){
+        return args -> {
+//            RequestContext.setUserId(0L);
+//            var userRole = new RoleEntity();
+//            userRole.setName(Authority.USER.name());
+//            userRole.setAuthorities(Authority.USER);
+//            roleRepository.save(userRole);
+//
+//            var adminRole = new RoleEntity();
+//            adminRole.setName(Authority.ADMIN.name());
+//            adminRole.setAuthorities(Authority.ADMIN);
+//            roleRepository.save(adminRole);
+//
+//            RequestContext.start();
+        };
+    }
+
 }
+/*
+Schema file 4:36
+ */
---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, user_id, first_name, last_name, email, phone, bio, reference_id, image_url, created_by, updated_by, created_at, updated_at, account_non_expired, account_non_locked, enabled, mfa)
+VALUES (0, '023a7479-e7a7-079f-3ae5-a766fe25eca9', 'System', 'System', 'system@gmail.com', '1234567890',
+        'This is not a user but the system itself', '897f3898-31ed-654a-eb93-531648a8db88',
+        'https://cdn-icons-pgn.flaticon.com/128/2911/2911833.png', 0, 0, '2024-09-27 02:15:45.754123', '2024-09-27 02:15:45.754123',
+        true, true, false, false);
